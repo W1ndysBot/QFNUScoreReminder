64ab8c5b1e908f8e34ad44d9d39b01e9ef5ff1f5
W1ndys <w1ndys@outlook.com>
2025-01-10 11:35:31 +0800

    修改环境配置

--- app/linux_build_and_run.sh
+++ app/linux_build_and_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 设置项目名称和镜像名称
-PROJECT_NAME="qfnu-login-webserver"
-IMAGE_NAME="qfnu-login-webserver"
+PROJECT_NAME="qfnu-score-reminder-webserver"
+IMAGE_NAME="qfnu-score-reminder-webserver"
 
 # 构建Docker镜像
 echo "Building Docker image..."
@@ -19,7 +19,7 @@ docker rm -f $PROJECT_NAME
 
 # 运行Docker容器
 echo "Running Docker container..."
-docker run -d -p 5000:5000 -v "$(pwd)":/app --name $PROJECT_NAME $IMAGE_NAME
+docker run -d -p 5001:5001 -v "$(pwd)":/app --name $PROJECT_NAME $IMAGE_NAME
 
 # 检查容器是否成功启动
 if [ $? -ne 0 ]; then
@@ -27,4 +27,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Docker container is running. Access the application at http://localhost:5000" 
+echo "Docker container is running. Access the application at http://localhost:5001" 
--- app/win_build_and_run.sh
+++ app/win_build_and_run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 设置项目名称和镜像名称
-PROJECT_NAME="qfnu-login-webserver"
-IMAGE_NAME="qfnu-login-webserver"
+PROJECT_NAME="qfnu-score-reminder-webserver"
+IMAGE_NAME="qfnu-score-reminder-webserver"
 
 # 构建Docker镜像
 echo "Building Docker image..."
@@ -16,7 +16,7 @@ fi
 
 # 运行Docker容器
 echo "Running Docker container..."
-docker run -d -p 5000:5000 -v "/${PWD//\\//}":/app --name $PROJECT_NAME $IMAGE_NAME
+docker run -d -p 5001:5001 -v "/${PWD//\\//}":/app --name $PROJECT_NAME $IMAGE_NAME
 
 # 检查容器是否成功启动
 if [ $? -ne 0 ]; then
@@ -24,4 +24,4 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-echo "Docker container is running. Access the application at http://localhost:5000"
+echo "Docker container is running. Access the application at http://localhost:5001"
